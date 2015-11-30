95efdb889fe291a6516a551c944b1ac1baf52eac
Tom Conant <thomasaconant@gmail.com>
2015-11-29 19:55:08 -0500

    site folder created

---
+++ site/scripts/coinflip.js
@@ -0,0 +1,64 @@
+var total = {
+  heads:0,
+  tails:0
+};
+ 
+ var flip = function () {
+   random = Math.floor(Math.random() * 2);
+   if (random === 1) {
+     return "Heads";
+   } else {
+     return "Tails";
+   }
+ };
+
+  var flipMany = function (a) {
+    for (var i = 0; i < a; i ++) {
+      flip();
+      if (flip() === "Heads") {
+        total.heads += 1;
+      } else {
+        total.tails += 1;
+      }
+    }
+  };
+
+
+
+
+ $(document).ready(function() {
+
+
+  $(".oneT").on('click', function() {
+    flipMany(1);
+    console.log(total);
+    $('.ms-headsNum').html(total.heads);
+    $('.ms-tailsNum').html(total.tails);
+    console.log("Its working!");
+  });
+
+  $(".tenT").on('click', function() {
+    flipMany(10);
+    console.log(total);
+    $('.ms-headsNum').html(total.heads);
+    $('.ms-tailsNum').html(total.tails);
+    console.log("Its working!");
+  });
+
+  $(".billT").on('click', function() {
+    flipMany(100);
+    console.log(total);
+    $('.ms-headsNum').html(total.heads);
+    $('.ms-tailsNum').html(total.tails);
+    console.log("Its working!");
+  });
+
+  $(".reset").on('click', function() {
+    $('.ms-headsNum').html('0');
+    $('.ms-tailsNum').html('0');
+    total.heads = 0;
+    total.tails = 0;
+    console.log("Its working!");
+  });
+
+});
